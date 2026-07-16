6b2159838213c50eb3c4e64bbd416810cabdeeae
Max Schulberg <max.schulberg@specter.co>
2026-07-16 13:19:00 -0700

    updated installer

--- install.sh
+++ install.sh
@@ -1,4 +1,6 @@
+#!/usr/bin/env bash
 
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 INSTALLPATH="${HOME}/.local/bin"
 
 
@@ -18,10 +20,15 @@ else
   extra="run source ~/.zshrc to update PATH (future logins you don't need to)"
 fi
 
-cp pingeroo $INSTALLPATH
-
-echo "install complete.  you can remove the pingeroo folder."
-echo $extra
-echo
-echo "Usage:  pingeroo 8.8.8.8 &"
+echo "copying pingeroo to $INSTALLPATH"
+cp ${SCRIPT_DIR}/pingeroo ${INSTALLPATH}
+if [[ $? -eq 0 ]]; then
+    echo -e "\e[1;32mSuccess\e[0m"
+    echo "install complete.  you can remove $SCRIPT_DIR"
+    echo $extra
+    echo
+    echo "Usage:  pingeroo 8.8.8.8 &"
+else
+    echo -e"\e[1;33mfailed to copy.  not installed!\e[0m"
+fi
 
